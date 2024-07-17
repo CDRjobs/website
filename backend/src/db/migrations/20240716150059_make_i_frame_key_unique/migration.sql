534d5de335c700ba50505115282e4c32f1439968
Pierre NOËL <contact@pierre-noel.fr>
2024-07-17 15:26:25 +0200

    reset database

--- backend/src/db/migrations/20240716150059_make_i_frame_key_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[iFrameKey]` on the table `Client` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Client_iFrameKey_key" ON "Client"("iFrameKey");
--- backend/src/db/migrations/20240716195029_add_xxxl_to_company_size/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "CompanySize" ADD VALUE 'xxxl';
